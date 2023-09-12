6cffa969506cd1fabc3a94cacd4fa08df5c08be2
ellenkh <92035794+ellenkh@users.noreply.github.com>
2023-09-12 09:25:20 +0200

    Update use-mq2-gas-sensor.ino

--- labs/week37/sketches/use-mq2-gas-sensor/use-mq2-gas-sensor.ino
+++ labs/week37/sketches/use-mq2-gas-sensor/use-mq2-gas-sensor.ino
@@ -1,24 +1,26 @@
+//This code is designed for a system detection smoke through digital setup
+
+//Define pin number for the smoke sensor  
 int smokePinD = 2;
-int buzzerPin = 8;
-int LEDPin = 4;
 
 void setup() {
+  //Start the serial communication at a baud rate of 19200
   Serial.begin(19200);
-  pinMode(smokePinD, INPUT);
-  pinMode(LEDPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
+  //Set the pin mode for the smoke sensor
+  pinMode(smokePinD, INPUT);  
+  //delay for 5 seconds to allow initialization
   delay(5000);
 }
 
 void loop() {
+  //Check the digital state of the smoke sensor pin
   if (digitalRead(smokePinD) == HIGH) {
-    Serial.println("Warning, value high");
-    digitalWrite(LEDPin, HIGH);
-    tone(buzzerPin, 880);
+    //If the smoke sensor reads HIGH, indicate smoke is detected
+    Serial.println("Warning, value high");//Print a warning message    
   } else {
-    Serial.println("Normal values");
-    digitalWrite(LEDPin, LOW);
-    noTone(buzzerPin);
+    //If the smoke sensor reads LOW, indicate no smoke is detected
+    Serial.println("Normal values"); //Print a normal message
   }
+  //Delay for 5 seconds before checking again
   delay(5000);
 }
