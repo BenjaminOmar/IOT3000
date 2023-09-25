eae97d50d5d13dd4dd684061a15eafaaf862062f
Antonin De Jesus <antonin.dejesus@outlook.com>
2023-09-25 23:20:17 +0200

    Add script for building string from HTML file

---
+++ other/arduino_string_builder.py
@@ -0,0 +1,36 @@
+import re
+import argparse
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument("-n", "--name", default="s")
+    parser.add_argument("-o", "--oneline", action="store_true")
+    args = parser.parse_args()
+    print(build_code_lines(args.path, args.name, args.oneline))
+
+def build_code_lines(filepath: Path, name: str, oneline: bool) -> str:
+    with open(filepath) as f:
+        lines = f.read().splitlines()
+    if oneline:
+        return f"{name} = {''.join(map(format_line, lines))}"
+    return "\n".join(
+        [f"{name} = {format_line(lines[0])}"]
+        + [
+            f"{name} += \"{format_line(line)}\""
+            for line in lines[1:]
+        ]
+    )
+
+def format_line(text: str) -> str:
+    expressions = [
+        (r"[\r\t]", ""),
+        (r"^\s{2,}", "")
+    ]
+    for exp, replace in expressions:
+        text = re.sub(exp, replace, text)
+    return text
+
+if __name__ == "__main__":
+    main()
