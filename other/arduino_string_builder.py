1525965ec2f2b1e0ac1662d5262fde7aeeeafb54
Antonin De Jesus <antonin.dejesus@outlook.com>
2023-10-02 01:41:40 +0200

    Fix output code formatting

--- other/arduino_string_builder.py
+++ other/arduino_string_builder.py
@@ -14,11 +14,11 @@ def build_code_lines(filepath: Path, name: str, oneline: bool) -> str:
     with open(filepath) as f:
         lines = f.read().splitlines()
     if oneline:
-        return f"{name} = {''.join(map(format_line, lines))}"
+        return f"{name} = {''.join(map(format_line, lines))};"
     return "\n".join(
-        [f"{name} = {format_line(lines[0])}"]
+        [f"{name} = \"{format_line(lines[0])}\";"]
         + [
-            f"{name} += \"{format_line(line)}\""
+            f"{name} += \"{format_line(line)}\";"
             for line in lines[1:]
         ]
     )
@@ -26,7 +26,8 @@ def build_code_lines(filepath: Path, name: str, oneline: bool) -> str:
 def format_line(text: str) -> str:
     expressions = [
         (r"[\r\t]", ""),
-        (r"^\s{2,}", "")
+        (r"^\s{2,}", ""),
+        (r"\"", "'")
     ]
     for exp, replace in expressions:
         text = re.sub(exp, replace, text)
